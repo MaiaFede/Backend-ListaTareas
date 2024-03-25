443ce4403d00598299881656563159058264c765
Maia Federica_G <maiafedericag@gmail.com>
2024-03-25 11:10:06 -0300

    Añadi el middleware static para cargar el index.html en la ruta principal del servidor

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import "dotenv/config";
 import cors from "cors";
 import morgan from "morgan";
 import tareaRouter from "./src/routes/tareas.routes.js";
+import path from "path";
+import { fileURLToPath } from 'url';
 
 const app = express();
 app.set('port', process.env.PORT || 4000);
@@ -14,5 +16,11 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.info(__filename)
+console.info(path.join(__dirname, '/public'));
+app.use(express.static(path.join(__dirname, '/public')))
 
 app.use("/api", tareaRouter);
