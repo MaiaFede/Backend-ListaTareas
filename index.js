9e902290e16422b4f59e4bab477d2fb383730c13
Maia Federica_G <maiafedericag@gmail.com>
2024-03-25 11:03:06 -0300

    Enroute y cree la funcion listar tareas para hacer el get de la lista

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import morgan from "morgan";
+import tareaRouter from "./src/routes/tareas.routes.js";
 
 const app = express();
 app.set('port', process.env.PORT || 4000);
 app.listen(app.get('port'), ()=>{
-    console.log("Estoy en el puerto "+ app.get("port"))
+    console.info("Estoy en el puerto "+ app.get("port"))
 })
 
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
+app.use("/api", tareaRouter);
---
+++ src/controllers/tareas.controllers.js
@@ -0,0 +1,4 @@
+export const listarTareas= (req, res)=>{
+    console.info("aqui obtener la lista de todos los productos");
+    res.send("Aqui enviaremos ls lista de productos")
+}
