c8ac8c6a02ad8ea275fa9106ae0b81f9c76c7f74
Maia Federica_G <maiafedericag@gmail.com>
2024-03-25 09:36:17 -0300

    elegi un puerto para correr el backend

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 import express from "express";
 
-console.log("hola mundo")
+const app = express();
+app.set('port', process.env.PORT || 4000);
+app.listen(app.get('port'), ()=>{console.log("Estoy en el puerto")})
